e32c6831ca157c1e45ae056ef66561ce26ab5346
amarchenko700 <lexx700@yandex.ru>
2021-09-16 18:24:33 +0300

    lesson 3: разработал класс Calculator, который отвечает за все расчетные операции

--- app/src/main/java/com/example/calculator/Calculator.java
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -1,55 +1,104 @@
 package com.example.calculator;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import java.io.Serializable;
 
-public class Calculator implements Parcelable {
+import MathOperations.*;
+
+public class Calculator implements Serializable {
 
     private String expressionString;
-    private Float memoryValue = 0f;
+    private Float memoryValue;
+    private MathGB mathGB;
 
     public Calculator(){
-
+        mathGB = new MathGB();
+        expressionString = "";
+        memoryValue = 0f;
     }
 
-    // region Parcelable_metod
-    protected Calculator(Parcel in) {
-        expressionString = in.readString();
-        if (in.readByte() == 0) {
-            memoryValue = null;
-        } else {
-            memoryValue = in.readFloat();
+    public void addExpression(String inputExpressionString){
+        expressionString += inputExpressionString;
+    }
+
+    public void skipMemory() {
+        memoryValue = 0f;
+    }
+
+    public void setValueFromMemory() {
+        expressionString = Float.toString(memoryValue);
+    }
+
+    public void putInMemory(int koef){
+        memoryValue = memoryValue + getLastNumberFloat() * koef;
+    }
+
+    public void clearInput() {
+        char[] expCharArr = expressionString.toCharArray();
+        if (expCharArr.length > 0) {
+            expressionString = String.copyValueOf(expCharArr, 0, expCharArr.length - 1);
         }
     }
 
-    public static final Creator<Calculator> CREATOR = new Creator<Calculator>() {
-        @Override
-        public Calculator createFromParcel(Parcel in) {
-            return new Calculator(in);
-        }
-
-        @Override
-        public Calculator[] newArray(int size) {
-            return new Calculator[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(expressionString);
-        if (memoryValue == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeFloat(memoryValue);
+    public void clearLastOperation() {
+        char[] expCharArr = expressionString.toCharArray();
+        int indexOperation;
+        for (indexOperation = expCharArr.length - 1; indexOperation >= 0; indexOperation--) {
+            char currentChar = expCharArr[indexOperation];
+            if (!mathGB.charIsNumber(currentChar)) {
+                expressionString = String.copyValueOf(expCharArr, 0, indexOperation + 1);
+                break;
+            }
         }
     }
-    // endregion
+
+    public void evaluate() {
+        mathGB.setExpression(expressionString);
+        Float result = mathGB.evaluate();
+        expressionString = Float.toString(result);
+    }
+
+    private String getLastNumberString(){
+        String lastNumber = "";
+        char[] expCharArr = expressionString.toCharArray();
+        int indexOperation;
+        for (indexOperation = expCharArr.length - 1; indexOperation >= 0; indexOperation--) {
+            char currentChar = expCharArr[indexOperation];
+            if (mathGB.charIsNumber(currentChar)) {
+                lastNumber = currentChar + lastNumber;
+            }else {
+                break;
+            }
+        }
+        return lastNumber;
+    }
+
+    public boolean lastCharIsNumber(){
+        char lastChar = getLastChar();
+        boolean isItNumber;
+        if (mathGB.charIsNumber(lastChar) && lastChar != '.'){
+            isItNumber = true;
+        }else {
+            isItNumber = false;
+        }
+        return isItNumber;
+    }
+
+    private Float getLastNumberFloat(){
+        Float lastNumber = 0f;
+        String lastNumberString = getLastNumberString();
+        lastNumber = Float.parseFloat(lastNumberString);
+        return lastNumber;
+    }
+
+    private char getLastChar(){
+        char[] expCharArr = expressionString.toCharArray();
+        char lastChar = expCharArr[expCharArr.length - 1];
+        return lastChar;
+    }
+
+    public void resetExpression(){
+        expressionString = "";
+    }
 
     public Float getMemoryValue() {
         return memoryValue;
