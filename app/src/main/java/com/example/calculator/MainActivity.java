90f79be51819dee0a0f2e5855ea6631b19d9a1ca
amarchenko700 <lexx700@yandex.ru>
2021-09-16 18:24:06 +0300

    lesson 3: сделал обработку нажатий кнопок, сохранение при повороте

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,8 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,12 +14,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView expression_string_textview, text_memory_textview;
     private Calculator calculator;
 
-    private final int[] arrayNumberButton = new int[]{R.id.number_0_button, R.id.number_1_button,
+    private final int[] arrayCalculatorButtonId = new int[]{R.id.number_0_button, R.id.number_1_button,
             R.id.number_2_button, R.id.number_3_button, R.id.number_4_button, R.id.number_5_button,
             R.id.number_6_button, R.id.number_7_button, R.id.number_8_button, R.id.number_9_button};
-
-    private final int[] numberOperationIds = new int[]{R.id.addition_button, R.id.subtraction_button,
-            R.id.multiplication_button, R.id.division_button};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +29,146 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             calculator = new Calculator();
         } else {
-            calculator = savedInstanceState.getParcelable(OBJECT_CALCULATOR);
+            calculator = (Calculator) savedInstanceState.getSerializable(OBJECT_CALCULATOR);
             expression_string_textview.setText(calculator.getExpressionString());
         }
 
+        setVisibleTextMemory();
+        setClickListeners(calculator);
+    }
+
+    private void setVisibleTextMemory() {
         if (calculator.getMemoryValue() == 0) {
             text_memory_textview.setVisibility(View.INVISIBLE);
         } else {
             text_memory_textview.setVisibility(View.VISIBLE);
         }
+    }
+
+    //region Установка_Listener
+    private void setClickListeners(Calculator calculator) {
+
+        CalcButtonListener calcButtonListener = new CalcButtonListener(calculator);
+
+        setClickListenerOnButton(R.id.memory_clear_button, calcButtonListener);
+        setClickListenerOnButton(R.id.memory_read_button, calcButtonListener);
+        setClickListenerOnButton(R.id.memory_addition_button, calcButtonListener);
+        setClickListenerOnButton(R.id.memory_subtraction_button, calcButtonListener);
+
+        setClickListenerOnButton(R.id.clear_entry_button, calcButtonListener);
+        setClickListenerOnButton(R.id.clear_button, calcButtonListener);
+        setClickListenerOnButton(R.id.delete_button, calcButtonListener);
+
+        setClickListenerOnButton(R.id.equally_button, calcButtonListener);
+        setClickListenerOnButton(R.id.dot_button, calcButtonListener);
+
+        setClickListenerOnButton(R.id.addition_button, calcButtonListener);
+        setClickListenerOnButton(R.id.subtraction_button, calcButtonListener);
+        setClickListenerOnButton(R.id.multiplication_button, calcButtonListener);
+        setClickListenerOnButton(R.id.division_button, calcButtonListener);
+
+        for (int numberOperationId : arrayCalculatorButtonId) {
+            findViewById(numberOperationId).setOnClickListener(v -> {
+                Button calculatorButton = (Button) v;
+                calculator.addExpression(calculatorButton.getText().toString());
+                expression_string_textview.setText(calculator.getExpressionString());
+            });
+        }
 
     }
 
+    private void setClickListenerOnButton(int idView, CalcButtonListener calcButtonListener) {
+        if (findViewById(idView) != null)
+            findViewById(idView).setOnClickListener(calcButtonListener);
+    }
+
+    class CalcButtonListener implements View.OnClickListener {
+
+        private Calculator calculator;
+
+        public CalcButtonListener(Calculator calculator) {
+            this.calculator = calculator;
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                //region memory
+                case R.id.memory_clear_button: {
+                    calculator.skipMemory();
+                    setVisibleTextMemory();
+                    break;
+                }
+                case R.id.memory_read_button: {
+                    calculator.setValueFromMemory();
+                    break;
+                }
+                case R.id.memory_addition_button: {
+                    calculator.putInMemory(1);
+                    setVisibleTextMemory();
+                    break;
+                }
+                case R.id.memory_subtraction_button: {
+                    calculator.putInMemory(-1);
+                    setVisibleTextMemory();
+                    break;
+                }
+                //endregion
+                case R.id.clear_button: {
+                    calculator.resetExpression();
+                    break;
+
+                }
+                case R.id.clear_entry_button: {
+                    calculator.clearLastOperation();
+                    break;
+                }
+                case R.id.delete_button: {
+                    calculator.clearInput();
+                    break;
+                }
+                case R.id.equally_button: {
+                    calculator.evaluate();
+                    break;
+                }
+                case R.id.dot_button: {
+                    addExpressionIfLastCharNumber(".");
+                    break;
+                }
+                case R.id.addition_button:{
+                    addExpressionIfLastCharNumber("+");
+                    break;
+                }
+                case R.id.subtraction_button:{
+                    addExpressionIfLastCharNumber("-");
+                    break;
+                }
+                case R.id.multiplication_button:{
+                    addExpressionIfLastCharNumber("*");
+                    break;
+                }
+                case R.id.division_button:{
+                    addExpressionIfLastCharNumber("/");
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+            expression_string_textview.setText(calculator.getExpressionString());
+        }
+    }
+    //endregion
+
+    private void addExpressionIfLastCharNumber(String expression){
+        if (calculator.lastCharIsNumber()){
+            calculator.addExpression(expression);
+        }
+    }
+
     @Override
-    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
-        outState.putParcelable(OBJECT_CALCULATOR, calculator);
-        super.onSaveInstanceState(outState, outPersistentState);
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(OBJECT_CALCULATOR, calculator);
     }
 }
